abfdaff0b666998a8b34154c1b3c2ad05a08c4c3
akhil <f20180458@pilani.bits-pilani.ac.in>
2021-09-23 20:12:11 +0530

    TataIscon added

--- src/main/java/parsers/tataiscon/TataisconCrawlingService.java
+++ src/main/java/parsers/tataiscon/TataisconCrawlingService.java
@@ -8,10 +8,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class TataisconCrawlingService {
     public List<CrawlCommodityPriceDto> handleRequest() {
-        HttpRequestDto httpRequestDto = buildRequest("");
+        HttpRequestDto httpRequestDto = buildRequest();
         HttpClientPool httpClientPool = new HttpClientPool();
         try {
             HttpResponseDto responseDto = httpClientPool.executeRequest(httpRequestDto);
@@ -20,25 +21,64 @@ public class TataisconCrawlingService {
                         httpRequestDto + responseDto);
                 return new ArrayList<>();
             }
-            System.out.println(responseDto.getResponseString());
+            TataisconParser tataisconParser = new TataisconParser();
+            Set<String>states=tataisconParser.getStates(responseDto.getResponseString());
+            for(String state:states){
+                httpRequestDto=buildRequest(state);
+                responseDto=httpClientPool.executeRequest(httpRequestDto);
+                if (!responseDto.getSuccessful()) {
+                    System.out.println("error while getting response for enam reqeuset [{}] , response [{}]" +
+                            httpRequestDto + responseDto);
+                    continue;
+                }
+                Set<String>districts=tataisconParser.getDistricts(responseDto.getResponseString());
+                for(String district:districts){
+                    httpRequestDto=buildRequest(state,district);
+                    responseDto=httpClientPool.executeRequest(httpRequestDto);
+                    if (!responseDto.getSuccessful()) {
+                        System.out.println("error while getting response for enam reqeuset [{}] , response [{}]" +
+                                httpRequestDto + responseDto);
+                        continue;
+                    }
+                    tataisconParser.ParseCommodityPrice(responseDto.getResponseString(),state,district);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("error while getting data  for domain {}" + CommodityPriceSource.TATAISCON);
         }
         return new ArrayList<>();
     }
-    public HttpRequestDto buildRequest(String state){
-        String url = "https://tatatiscon.co.in/services/service.php?pagename=rcp_state_change";
-        String payload = "state=Assam&";
-
+    public HttpRequestDto buildRequest(String state,String district){
+        String url = "https://tatatiscon.co.in/services/service.php?pagename=rcp_data";
+        String payload=String.format("rcp_state=%s&rcp_district=%s&rcp_product=TATA+TISCON+SUPER+DUCTILE+REBAR",state,district);
         Map<String, String> headers = HttpHeaderUtils.getApplicationFormURLEncodedHeaders();
-        headers.put("X-Requested-With","XMLHttpRequest");
-
         return HttpRequestDto.Builder.httpRequestDto()
                 .withRequestType(RequestType.POST)
                 .withUrl(url)
                 .withHeaders(headers)
                 .withPayload(payload)
+                .build();
+    }
+    public HttpRequestDto buildRequest(String state){
+        String url = "https://tatatiscon.co.in/services/service.php?pagename=rcp_state_change";
+        String payload = "state="+state;
+        Map<String, String> headers = HttpHeaderUtils.getApplicationFormURLEncodedHeaders();
+        return HttpRequestDto.Builder.httpRequestDto()
+                .withRequestType(RequestType.POST)
+                .withUrl(url)
+                .withHeaders(headers)
+                .withPayload(payload)
+                .build();
+    }
+    public HttpRequestDto buildRequest(){
+        String url="https://tatatiscon.co.in/rcp.php";
+        Map<String, String> headers = HttpHeaderUtils.getApplicationFormURLEncodedHeaders();
+        return HttpRequestDto.Builder.httpRequestDto()
+                .withRequestType(RequestType.GET)
+                .withUrl(url)
+                .withHeaders(headers)
+                .withPayload(null)
                 .build();
     }
 }
